4c8dbda2fe0bc5df887e8f808b54996e19c9b6d6
Ozioma <ogbeoziomajnr@yahoo.com>
2021-10-26 12:20:11 +0700

    Update ManipulatingBits.kt

--- src/main/kotlin/bitwiseandbinaryoperations/ManipulatingBits.kt
+++ src/main/kotlin/bitwiseandbinaryoperations/ManipulatingBits.kt
@@ -7,7 +7,7 @@ class BitManipulator {
         return (1L shl position).inv() and number
     }
 
-    fun bitAtPositionIsSet(position: Int, number: Long, numberOfBits: Int): Boolean {
+    fun isBitAtPositionSet(position: Int, number: Long, numberOfBits: Int): Boolean {
         return ((1 shl numberOfBits - position).toLong() and number) > 0
     }
 }
